bf34516200dae15ac5fd0341c28876cbf77f37c8
werasak mayer <jokerwwe0542@gmail.com>
2023-07-19 16:05:12 +0700

    ok

--- src/app/page.js
+++ src/app/page.js
@@ -1,51 +1,28 @@
+import { Footer } from "../../components/Footer";
+import { Task } from "../../components/Task";
+import { TaskInput } from "../../components/TaskInput";
+import { Header } from "../../components/Header";
+
 export default function Home() {
   return (
     // Main container
     <div className="container mx-auto">
       {/* header section */}
-      <div>
-        <h4 className="display-4 fst-italic text-center">Todo List Mock Up</h4>
-        <p className="text-secondary text-center">
-          This lab teach you how to split react component(s)
-        </p>
-      </div>
-
+      <Header />
       {/* tasks container */}
       <div style={{ maxWidth: "400px" }} className="mx-auto">
         {/* task input */}
-        <div className="d-flex gap-1">
-          <input
-            className="form-control"
-            placeholder="Insert a task here.."
-          ></input>
-          <button className="btn btn-primary">Add</button>
-        </div>
+        <TaskInput />
         {/* task */}
-        <div className="d-flex p-3 gap-2 align-items-center border-bottom">
-          <span>Read a book</span>
-          <button className="btn btn-success">Done</button>
-          <button className="btn btn-danger">Delete</button>
-        </div>
+        <Task title="Read a book" />
         {/* task */}
-        <div className="d-flex p-3 gap-2 align-items-center border-bottom">
-          <span>Take a shower</span>
-          <button className="btn btn-success">Done</button>
-          <button className="btn btn-danger">Delete</button>
-        </div>
+        <Task title="Take a shower" />
         {/* task */}
-        <div className="d-flex p-3 gap-2 align-items-center border-bottom">
-          <span>Sleep</span>
-          <button className="btn btn-success">Done</button>
-          <button className="btn btn-danger">Delete</button>
-        </div>
+        <Task title="Sleep" />
       </div>
 
       {/* //footer section */}
-      <div className="mt-3">
-        <p className="text-secondary text-center">
-          copyright © 2023 Chayanin Suatap 12345678
-        </p>
-      </div>
+      <Footer year="2023" fullname="Werasak Mayer" studentId="650610808" />
     </div>
   );
 }
